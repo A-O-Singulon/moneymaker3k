3f719d717254778eabe7f48c3f3b02b05ac64cad
JOoão Pedro <aguiarhernandez4@gmail.com>
2025-10-04 22:13:20 -0300

    Nosso Primeiro Commit

---
+++ FazedorDeDinheiro3000/src/main/java/com/example/FazedorDeDinheiro3000/FazedorDeDinheiro3000Application.java
@@ -0,0 +1,13 @@
+package com.example.FazedorDeDinheiro3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazedorDeDinheiro3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazedorDeDinheiro3000Application.class, args);
+	}
+
+}
---
+++ FazedorDeDinheiro3000/src/test/java/com/example/FazedorDeDinheiro3000/FazedorDeDinheiro3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FazedorDeDinheiro3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazedorDeDinheiro3000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
